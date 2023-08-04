20c2ac95ea7266e6828956ef66515f6fbda6e816
Emmanuel Ngoka <emmanuelngoka778@gmail.com>
2023-08-04 22:28:10 +0100

    added text color update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,27 +6,33 @@ import WordList from "./WordList.vue";
 
 const lastLetter = ref("");
 const currentWord = ref("");
-const typedWords = reactive<string[]>([]);
+const typedWords = reactive<{ words: string[] }>({ words: [] });
 
 function changeLast(e: KeyboardEvent) {
   lastLetter.value = e.key;
-  if (e.code === "Space") {
-    typedWords.push(currentWord.value);
-    currentWord.value = "";
-    console.log(typedWords);
-  }
+}
+
+function spaceFunction(e: KeyboardEvent) {
+  lastLetter.value = e.key;
+  typedWords.words.push(currentWord.value);
+  currentWord.value = "";
+  console.log(typedWords.words);
 }
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center">
-    <WordList :typed-words="typedWords" />
+    <WordList :typed-words="typedWords.words" />
     <div class="p-10">
       <input
         v-model="currentWord"
+        @input="e => {if((e.target as HTMLInputElement).value === ' '){
+            currentWord = ''
+        }}"
         spellcheck="false"
         autofocus
         class="bg-transparent w-[500px] text-center tracking-wide azeret focus:outline-none text-3xl text-white"
+        @keydown.space="spaceFunction"
         @keydown="changeLast"
       />
     </div>
--- src/components/WordList.vue
+++ src/components/WordList.vue
@@ -10,7 +10,11 @@ const wordList = generate(200);
     class="flex mt-10 text-white/40 flex-wrap gap-x-5 gap-y-2.5 azeret text-2xl w-[80vw] h-20 overflow-hidden"
   >
     <span
-      :class="{ 'text-lime-400': props.typedWords?.[i] === wordList[i] }"
+      :class="{
+        'text-lime-400': props.typedWords?.[i] === wordList[i],
+        'text-red-400':
+          props.typedWords?.[i] !== wordList[i] && props.typedWords.length > i,
+      }"
       v-for="(word, i) in wordList"
       :key="i"
       >{{ word }}</span
